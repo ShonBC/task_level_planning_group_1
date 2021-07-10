b495e329d1d18799533ed607a85945bc7b7b55aa
ShonBC <scortes3@umd.edu>
2021-07-09 20:55:48 -0700

    created execute_plan function

--- execution/execution.py
+++ execution/execution.py
@@ -7,8 +7,17 @@ def read_plan():
         # Place each line as an element in the plan list. Use ".rstrip" to remove white space.
         plan = [line.strip() for line in file]
 
+    return plan
+
+def execute_plan(plan):
+
+    for command in plan:
+        pass
+
+
 task.main()
 
+execute_plan(read_plan())
 
 
 
