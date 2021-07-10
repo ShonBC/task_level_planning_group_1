50860bce469842b071f70c108ea901c8f35f0a0d
ShonBC <scortes3@umd.edu>
2021-07-09 20:38:22 -0700

    Add read_plan function

--- execution/execution.py
+++ execution/execution.py
@@ -1,5 +1,12 @@
 import taskplanning as task
 
+def read_plan():
+
+    with open('filename') as file:
+        
+        # Place each line as an element in the plan list. Use ".rstrip" to remove white space.
+        plan = [line.strip() for line in file]
+
 task.main()
 
 
