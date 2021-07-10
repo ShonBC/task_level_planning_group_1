6c5e4a3467314c4204d8d5ef5dc2df5e8c4d30cd
ShonBC <scortes3@umd.edu>
2021-07-10 11:42:33 -0700

    Started formatting execution function.

--- execution/execution.py
+++ execution/execution.py
@@ -1,23 +1,28 @@
 # from task_level_planning_group_1.execution 
 import taskplanning as task
 import planning.fileio
-import classes.industrial_robot as industrial
-import classes.ground_robot as ground
-import classes.gantry_robot as gantry
+import classes.industrial_robot as Industrial
+import classes.ground_robot as Ground
+import classes.gantry_robot as Gantry
 
-# def read_plan():
 
-#     with open('filename') as file:
-        
-#         # Place each line as an element in the plan list. Use ".rstrip" to remove white space.
-#         plan = [line.strip() for line in file]
-
-#     return plan
-
-def execute_plan(plan):
+def execute_plan(plan, robot):
 
     for command in plan:
-        pass
+        if 'gantry' in command:
+            robot = 
+
+            if 'string' in command:
+                "Call robot.class.method"
+
+        elif 'ground' in command:
+            
+            if 'string' in command:
+                "Call robot.class.method"
+
+        elif 'string' in command:
+            "Call robot.class.method"
+    
 
     # print('kit complete')
 
