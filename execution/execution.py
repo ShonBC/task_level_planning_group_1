671e2972e885b62676410529cc18e6949bd14bd8
ShonBC <scortes3@umd.edu>
2021-07-10 12:49:28 -0700

    Added execute_plan statments

--- execution/execution.py
+++ execution/execution.py
@@ -6,23 +6,75 @@ import classes.ground_robot as Ground
 import classes.gantry_robot as Gantry
 
 
-def execute_plan(plan, robot):
+def execute_plan(plan, ind_robot, gnd_robot, gnt_robot, system_tracker):
 
     for command in plan:
-        if 'gantry' in command:
-            robot = 
+        
+        agv = system_tracker["agv"]["selected"]
 
-            if 'string' in command:
-                "Call robot.class.method"
+        #Check robot type
+        if 'gantry' in command:
+            robot = gnt_robot 
 
         elif 'ground' in command:
-            
-            if 'string' in command:
-                "Call robot.class.method"
+            robot = gnd_robot 
 
-        elif 'string' in command:
-            "Call robot.class.method"
-    
+        elif 'industrial' in command:
+            robot = gnd_robot
+
+        # Check for part type and bin number
+        if 'red' in command:
+            part_type = 'red battery'
+            bin_num = system_tracker["bins"]["red battery"]["location"]
+
+        elif 'blue battery' in command:
+            part_type = 'blue battery'
+            bin_num = system_tracker["bins"]["blue battery"]["location"]
+
+        elif 'blue sensor' in command:
+            part_type = 'blue sensor'
+            bin_num = system_tracker["bins"]["blue sensor"]["location"]
+
+        elif 'green' in command:
+            part_type = 'green regulator'
+            bin_num = system_tracker["bins"]["green regulator"]["location"]
+
+        if 'finger' in command:
+            gripper = 'finger gripper'
+        elif ' vacuum' in command:
+            gripper = 'vacuum gripper'
+
+        # Check for robot action
+        if 'pick' in command:
+            
+            robot.pick_up(part_type, bin_num)
+
+        elif 'put' in command:
+            
+            robot.put_down(part_type, bin_num)
+
+        elif 'attach' in command:
+            
+            robot.attach_gripper(gripper)
+
+        elif 'detach' in command:
+
+            robot.detach_gripper(gripper)
+
+        elif 'to bin' in command:
+
+            robot.move_to_bin(bin_num)
+        
+        elif 'to agv' in command:
+            robot.move_to_agv(system_tracker["agv"]["selected"]) 
+
+        elif 'to gripper' in command:
+            
+
+        elif 'from agv' in command:
+            robot.move_from_agv(system_tracker["agv"]["selected"])
+
+
 
     # print('kit complete')
 
